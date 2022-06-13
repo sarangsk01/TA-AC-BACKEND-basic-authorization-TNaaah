00cea089127f079155752898d85e223c3ebdf10e
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-06-13 18:47:22 +0530

    Assignment I

---
+++ block-BNaadz/auth-1/models/Comment.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema(
+  {
+    content: { type: String, required: true },
+    articleId: { type: Schema.Types.ObjectId, ref: 'Article', required: true },
+    author: String,
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Comment', commentSchema);
